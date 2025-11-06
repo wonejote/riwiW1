b0da327ab94d56d3e17e0ee8db8d9aa79901cc2b
wonejote <antonio.pulgarin97@gmail.com>
2025-11-06 06:50:10 -0500

    programa finalizado

--- inventario.py
+++ inventario.py
@@ -7,13 +7,14 @@ itemName = input("Nombre del producto: ") #itemName almacena el nombre del produ
 while  itemName.isdecimal() or itemName.isspace(): #bucle while, solo se entra si itemName conitene solo números o espacios
                                                    #Si se entra en el bucle, se vuelve a pedir al usuario un nombre de producto
                                                    #el bucle se repite hasta que itemName sea correcto
+                                                   #si el valor ingresado no es correcto, se explica el error al usuario
     itemName = input(f"{itemName} es inválido. No es posible nombrar productos solo con numeros o espacios \npor favor ingresa un nombre válido: ")
 
 itemPrice = input("Precio del producto: ") #itemPrice almacena el precio del producto
 
-while  itemPrice.isalpha() or itemPrice.isspace() or float(itemPrice) < 0: #bucle while, se puede entrar si sucede 1 de 3 casos:
-                                                                           # 1: tiene letras, tiene espacios, es negativo
-    itemPrice = input(f" {itemPrice} es inválido, por favor ingresa un precio numérico y positivo: ")
+while  not itemPrice.isdecimal():  #si el valor almacenado no es decilmal, se entra al bucle para volver a pedir un nuevo valor
+                                                                        
+    itemPrice = input(f" {itemPrice} es inválido, por favor ingresa un precio numérico y positivo: ") #si el valor ingresado no es correcto, se explica el error al usuario
 
 
 itemPrice = float(itemPrice)  #si se sale del bucle, es porque el usuario ingreso un valor correcto, entonces es posible 
@@ -21,18 +22,22 @@ itemPrice = float(itemPrice)  #si se sale del bucle, es porque el usuario ingres
 
 itemCount = input("Cantidad del producto: ") #itemCount almacena el numero de productos
 
-while not itemCount.isdecimal():
-    itemCount = input(f" {itemCount} es inválido, por favor ingresa una cantidad entera y positiva: ")
- 
+while not itemCount.isdecimal():  #funciana igual que itemPrice
+    itemCount = input(f" {itemCount} es inválido, por favor ingresa una cantidad entera y positiva: ") 
 itemCount = int(itemCount)
 
 #---------------------Task 3---------------------
 
-costoTotal = itemPrice*itemCount
+costoTotal = itemCount*itemPrice #costoTotal calcula es costo de tener "x" productos a "y" precio con la fórmula x*y
+                                 #como ya es seguro que "x" es int y  "y" float, no es necesario comprobar el tipo de costoTotal, será float
 
 #---------------------Task 4---------------------
 
-print(f"Producto: {itemName} | Precio: {itemPrice} | Cantidad: {itemCount} | Total: {costoTotal}")
+print(f"Producto: {itemName} | Precio: {itemPrice} | Cantidad: {itemCount} | Total: {costoTotal}") # se muesta al usuario la información de su producto 
+                                                                                                   # (nombre, precio, cantidad, costo total)
 
-#------------------------------------------------
+#---------------------Task 5---------------------
 
+#inventario.py es un programa que permita al usuario ingresar un único producto a un inventario. El programa
+#solicita nombre, precio y cantidad del producto, cada una de estas entradas cuenta con un sistema de validación
+#una vez todos los datos son válidos, el programa calcula el precio total del inventario y muesta toda la información al usuario.
